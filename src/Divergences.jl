77595987837fc1a5b5f5f02f9206ac087d4d8ade
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-07-29 20:10:54 +0200

    S

--- src/Divergences.jl
+++ src/Divergences.jl
@@ -1,11 +1,12 @@
 module Divergences
 
 using Distance
+importall Distance
 
 import Distance: get_common_len
+#import Base: gradient
 
 export
-    ##
     Divergence,
     KullbackLeibler,
     ReverseKullbackLeibler,
